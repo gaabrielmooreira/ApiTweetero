38cac88e48dcbf48e10cc3ade064384942d9bd43
gaabrielmooreira <gaabrielmooreira@gmail.com>
2023-01-06 16:16:46 -0300

    POST usuario /sign-up

--- src/app.js
+++ src/app.js
@@ -1,21 +1,19 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 const PORT = 5000;
 
-const usuario = {
-    username: 'bobesponja',
-    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
-}
+const usuarios = [];
 
-const tweet = {
-    username: "bobesponja",
-    tweet: "eu amo o hub"
-}
+const tweets = [];
 
-app.get("/sign-up", (req, res) =>{
-    
+app.post("/sign-up",(req, res) => {
+    const novoUsuario = req.body;
+    usuarios.push(novoUsuario);
+    console.log(usuarios);
+    res.send("OK");
 })
 
 app.listen(PORT, () => console.log("O servidor foi iniciado com sucesso."));
