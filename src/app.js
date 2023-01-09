10ed26440fe627df437505e9b57d689f6980c987
gaabrielmooreira <gaabrielmooreira@gmail.com>
2023-01-09 15:43:34 -0300

    Validação POST fix

--- src/app.js
+++ src/app.js
@@ -12,19 +12,20 @@ const users = [];
 const tweets = [];
 
 app.post("/sign-up",(req, res) => {
-    const {username,avatar} = req.body;
-    if((!username && typeof username !== "string") || (!avatar && typeof avatar !== "string")){
+    const newUser = req.body;
+    if((!newUser.username && typeof newUser.username !== "string") || (!newUser.avatar && typeof newUser.avatar !== "string")){
         return res.status(400).send("Todos os campos são obrigatórios!");
     } 
-    if(users.find((user) => user.username === username)) return res.status(400).send("Nome de usuario já existente.");
-    users.push({username,avatar});
+    if(users.find((user) => user.username === newUser.username)) return res.status(400).send("Nome de usuario já existente.");
+    users.push(newUser);
+    console.log(users);
     res.status(201).send("OK");
 })
 
 app.post("/tweets", (req, res) =>{
-    const {tweet, username} = req.body;
-    if(!tweet && typeof tweet !== "string" || !username && typeof username !== "string") return res.status(400).send("Todos os campos são obrigatórios!"); 
-    const findUsername = users.find((user) => user.username === username);
+    const infoTweet = req.body;
+    if(!infoTweet.tweet && typeof infoTweet.tweet !== "string" || !infoTweet.username && typeof infoTweet.username !== "string") return res.status(400).send("Todos os campos são obrigatórios!"); 
+    const findUsername = users.find((user) => user.username === infoTweet.username);
     if(!findUsername){
         return res.status(401).send("UNAUTHORIZED");;
     }
